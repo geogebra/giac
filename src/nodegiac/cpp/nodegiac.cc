7c3853d21f2207aee5f51a57cb0dcbf748995da6
zbynek <zbynek@23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e>
2019-12-09 14:27:49 +0000

    Update giac for Node 10+
    git-svn-id: https://dev.geogebra.org/svn/trunk/geogebra/giac@69383 23ce0884-8a58-47d3-bc5c-ddf1cd5b9f9e

--- src/nodegiac/cpp/nodegiac.cc
+++ src/nodegiac/cpp/nodegiac.cc
@@ -12,29 +12,30 @@ context ct;
 
 void Evaluate(const v8::FunctionCallbackInfo<Value>& args) {
   Isolate* isolate = Isolate::GetCurrent();
+  Local<Context> context = isolate->GetCurrentContext();
   HandleScope scope(isolate);
   std::string line_out;
 
-  v8::String::Utf8Value param1(args[0]->ToString());
+  v8::String::Utf8Value param1(isolate, args[0]->ToString(context).ToLocalChecked());
   std::string line_in = std::string(*param1);
 
   gen g;
   g=gen(line_in,&ct);
   try {
     line_out = giac::print(giac::eval(g,&ct),&ct);
-    }
-  catch (runtime_error & err) {
+  } catch (runtime_error & err) {
     line_out = std::string("ERROR: ") + err.what();
-    }
+  }
 
-  Handle<Value> line_out_v8 = String::NewFromUtf8( isolate, line_out.c_str() );
+  Local<Value> line_out_v8 = String::NewFromUtf8( isolate, line_out.c_str() );
   args.GetReturnValue().Set(String::NewFromUtf8(isolate,line_out.c_str()));
 }
 
-void Init(Handle<Object> exports) {
+void Initialize(Local<Object> exports) {
   Isolate* isolate = Isolate::GetCurrent();
-  exports->Set(String::NewFromUtf8(isolate, "evaluate"),
-      FunctionTemplate::New(isolate, Evaluate)->GetFunction());
+  Local<Context> context = isolate->GetCurrentContext();
+  exports->Set(context, String::NewFromUtf8(isolate, "evaluate", NewStringType::kNormal).ToLocalChecked(),
+      FunctionTemplate::New(isolate, Evaluate)->GetFunction(context).ToLocalChecked());
 }
 
-NODE_MODULE(giac, Init)
+NODE_MODULE(giac, Initialize)
